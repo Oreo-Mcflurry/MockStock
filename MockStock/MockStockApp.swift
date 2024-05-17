c7d74a06862c46cc24e960ee5c46c28e3db7e597
유인호 <yuiop1029@naver.com>
2024-05-17 09:36:40 +0900

    Initial Commit

---
+++ MockStock/MockStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockStockApp.swift
+//  MockStock
+//
+//  Created by A_Mcflurry on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MockStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
